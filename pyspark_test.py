d7e2d8b132c5610d91db3348faa02b38f680604e
김재익/데이터프로세싱 및 배치모니터링 <11st.PP48954@partner.sk.com>
2026-01-28 15:29:14 +0900

    update

--- pyspark_test.py
+++ pyspark_test.py
@@ -15,6 +15,34 @@ with DAG(
         name='spark-hive-ingestion-pod',
         namespace='airflow',
         image='apache/spark:3.4.2',
+        
+        # 1. 공용 공간(임시 볼륨) 생성
+        volumes=[
+            k8s.V1Volume(name='git-volume', emptyDir=k8s.V1EmptyDirVolumeSource())
+        ],
+        
+        # 2. 파일 먼저 받아오기 (Init Container)
+        init_containers=[
+            k8s.V1Container(
+                name="git-sync",
+                image="registry.k8s.io/git-sync/git-sync:v3.6.5", # git-sync 공식 이미지
+                env=[
+                    k8s.V1EnvVar(name="GIT_SYNC_REPO", value="https://github.com/jaeik714/airflow-dags.git"),
+                    k8s.V1EnvVar(name="GIT_SYNC_BRANCH", value="main"),
+                    k8s.V1EnvVar(name="GIT_SYNC_ROOT", value="/opt/airflow/dags"),
+                    k8s.V1EnvVar(name="GIT_SYNC_ONE_TIME", value="true"),
+                ],
+                volume_mounts=[
+                    k8s.V1VolumeMount(name="git-volume", mount_path="/opt/airflow/dags")
+                ]
+            )
+        ],
+        
+        # 3. 메인 Spark 컨테이너 설정
+        volume_mounts=[
+            k8s.V1VolumeMount(name="git-volume", mount_path="/opt/airflow/dags")
+        ],
+        
         cmds=["/bin/bash", "-c"],
         arguments=[
             "/opt/spark/bin/spark-submit "
@@ -28,7 +56,7 @@ with DAG(
             "--conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem "
             "--conf spark.hadoop.hive.metastore.uris=thrift://hive-metastore:9083 "
             "--conf spark.sql.warehouse.dir=s3a://warehouse/ "
-            # [주의] 자기 자신(pyspark_test.py)을 실행하도록 경로 설정
+            # 이제 파일이 존재하므로 직접 경로를 지정합니다!
             "/opt/airflow/dags/repo/pyspark_test.py" 
         ],
         is_delete_operator_pod=False,
