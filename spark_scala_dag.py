a3a5a37906996be768114b2e05e2bf75f3ae9843
김재익/데이터프로세싱 및 배치모니터링 <11st.PP48954@partner.sk.com>
2026-01-30 16:50:26 +0900

    update

--- spark_scala_dag.py
+++ spark_scala_dag.py
@@ -20,12 +20,23 @@ with DAG(
         startup_timeout_seconds=600,
         
         cmds=["/bin/bash", "-c"],
-        
-        # [핵심 2] 줄바꿈 없이 한 줄로 작성 (문법 에러 원천 차단)
         arguments=[
-            "/opt/spark/bin/spark-submit --master local[*] --conf spark.jars.ivy=/tmp/.ivy2 --packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 --conf spark.hadoop.fs.s3a.endpoint=http://minio.airflow.svc.cluster.local:9000 --conf spark.hadoop.fs.s3a.access.key=admin --conf spark.hadoop.fs.s3a.secret.key=password123 --conf spark.hadoop.fs.s3a.path.style.access=true --conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem --conf spark.hadoop.hive.metastore.uris=thrift://hive-metastore:9083 --conf spark.sql.warehouse.dir=s3a://warehouse/ --class com.example.spark.HiveTest s3a://jars/sparkscalahive_2.12-1.0.jar"
+            """
+            /opt/spark/bin/spark-submit \
+            --master local[*] \
+            --conf spark.jars.ivy=/tmp/.ivy2 \
+            --packages org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262 \
+            --conf spark.hadoop.fs.s3a.endpoint=http://minio.airflow.svc.cluster.local:9000 \
+            --conf spark.hadoop.fs.s3a.access.key=admin \
+            --conf spark.hadoop.fs.s3a.secret.key=password123 \
+            --conf spark.hadoop.fs.s3a.path.style.access=true \
+            --conf spark.hadoop.fs.s3a.impl=org.apache.hadoop.fs.s3a.S3AFileSystem \
+            --conf spark.hadoop.hive.metastore.uris=thrift://hive-metastore:9083 \
+            --conf spark.sql.warehouse.dir=s3a://warehouse/ \
+            --class com.example.spark.HiveTest \
+            s3a://jars/sparkscalahive_2.12-1.0.jar
+            """
         ],
-        
         is_delete_operator_pod=False,
         get_logs=True,
     )
